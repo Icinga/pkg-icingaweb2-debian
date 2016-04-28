c1aee5d6524505dd0969649d4595503e6c069a2d
Markus Frosch <lazyfrosch@debian.org>
2016-04-28 18:17:42 +0200

    Imported Upstream version 2.3.2

--- modules/monitoring/application/forms/Command/Object/ScheduleHostDowntimeCommandForm.php
+++ modules/monitoring/application/forms/Command/Object/ScheduleHostDowntimeCommandForm.php
@@ -3,6 +3,7 @@
 
 namespace Icinga\Module\Monitoring\Forms\Command\Object;
 
+use DateTime;
 use Icinga\Module\Monitoring\Command\Object\PropagateHostDowntimeCommand;
 use Icinga\Module\Monitoring\Command\Object\ScheduleHostDowntimeCommand;
 use Icinga\Module\Monitoring\Command\Object\ScheduleServiceDowntimeCommand;
@@ -61,6 +62,22 @@ class ScheduleHostDowntimeCommandForm extends ScheduleServiceDowntimeCommandForm
      */
     public function onSuccess()
     {
+        $end = $this->getValue('end')->getTimestamp();
+        if ($end <= $this->getValue('start')->getTimestamp()) {
+            $endElement = $this->_elements['end'];
+            $endElement->setValue($endElement->getValue()->format($endElement->getFormat()));
+            $endElement->addError($this->translate('The end time must be greater than the start time'));
+            return false;
+        }
+
+        $now = new DateTime;
+        if ($end <= $now->getTimestamp()) {
+            $endElement = $this->_elements['end'];
+            $endElement->setValue($endElement->getValue()->format($endElement->getFormat()));
+            $endElement->addError($this->translate('A downtime must not be in the past'));
+            return false;
+        }
+
         foreach ($this->objects as $object) {
             /** @var \Icinga\Module\Monitoring\Object\Host $object */
             if (($childHostsEl = $this->getElement('child_hosts')) !== null) {
